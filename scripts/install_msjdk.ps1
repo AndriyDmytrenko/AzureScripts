5075c9a1b10b3755036ec28fba9944ae7b89d4df
Andriy Dmytrenko <andriy.dmytrenko@ah.nl>
2021-10-05 19:38:17 +0300

    the script

--- scripts/install_msjdk.ps1
+++ scripts/install_msjdk.ps1
@@ -1,6 +1,3 @@
-Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
-
-scoop install git
-scoop bucket add java
-scoop install microsoft11-jdk
+Invoke-WebRequest https://aka.ms/download-jdk/microsoft-jdk-17.35.1-windows-x64.msi -UseBasicParsing -OutFile $Env:TEMP\microsoft-jdk-17.35.1-windows-x64.msi
+Start-Process -Wait -FilePath msiexec -ArgumentList /i, "$Env:TEMP\microsoft-jdk-17.35.1-windows-x64.msi", "'ADDLOCAL=\"FeatureMain,FeatureEnvironment,FeatureJarFileRunWith,FeatureJavaHome\"'", "'INSTALLDIR=\"C:\Program Files\Java\jdk-11\"'", /quiet, /norestart -Verb RunAs
 
