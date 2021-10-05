233308d900927df11346143ba6553f1f0b351b44
Andriy Dmytrenko <andriy.dmytrenko@ah.nl>
2021-10-05 19:47:07 +0300

    the script

--- scripts/install_msjdk.ps1
+++ scripts/install_msjdk.ps1
@@ -1,3 +1,3 @@
 Invoke-WebRequest https://aka.ms/download-jdk/microsoft-jdk-17.35.1-windows-x64.msi -UseBasicParsing -OutFile $Env:TEMP\microsoft-jdk-17.35.1-windows-x64.msi
-Start-Process -Wait -FilePath msiexec -ArgumentList /i, "$Env:TEMP\microsoft-jdk-17.35.1-windows-x64.msi", "'ADDLOCAL=\"FeatureMain,FeatureEnvironment,FeatureJarFileRunWith,FeatureJavaHome\"'", "'INSTALLDIR=\"C:\Program Files\Java\jdk-11\"'", /quiet, /norestart -Verb RunAs
+Start-Process -Wait -FilePath msiexec -ArgumentList /i, "$Env:TEMP\microsoft-jdk-17.35.1-windows-x64.msi", 'ADDLOCAL="FeatureMain,FeatureEnvironment,FeatureJarFileRunWith,FeatureJavaHome"', 'INSTALLDIR="C:\Program Files\Java\jdk-17\"', /quiet, /norestart -Verb RunAs
 
